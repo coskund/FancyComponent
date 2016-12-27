f63fc7210e776eb352b7215554c88d90050dac86
coskund <coskun_deniz_92@hotmail.com>
2016-12-28 01:09:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/cokundeniz/fancycomponent/actMain.java
@@ -0,0 +1,17 @@
+package com.example.cokundeniz.fancycomponent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class actMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_main);
+    }
+
+    private void Test (){
+        int i = 1 + 1 ;
+    }
+}
