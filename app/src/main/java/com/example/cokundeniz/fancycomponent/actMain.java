4864adef83ec399d77ba7796fe83a0eb85a91f11
coskund <coskun_deniz_92@hotmail.com>
2016-12-28 07:25:45 +0300

    deneme

--- app/src/main/java/com/example/cokundeniz/fancycomponent/actMain.java
+++ app/src/main/java/com/example/cokundeniz/fancycomponent/actMain.java
@@ -2,13 +2,22 @@ package com.example.cokundeniz.fancycomponent;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class actMain extends Activity {
+    RelativeLayout rlBackground;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_main);
+
+        BindControls();
+    }
+
+    private void BindControls() {
+        rlBackground = (RelativeLayout)findViewById(R.id.rlBackground);
     }
 
     private void Test (){
